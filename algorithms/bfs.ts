2437a078f16bfdefacc2be1401678eb9fd5c0c25
Amrit Sharma <amrit.sharma@bik.ai>
2023-08-19 01:08:40 +0530

    completed dijkstra and bfs algorithm visualization

---
+++ algorithms/bfs.ts
@@ -0,0 +1,70 @@
+//@ts-nocheck
+
+import { Queue } from "@/dataStructure/queue";
+
+export function BFS(matrix, startNode, endNode) {
+  console.log("BFS...");
+  const traversal = [];
+  const shortestPath = [];
+  // Use a map to store parent nodes for each visited node
+  const parentMap = new Map();
+
+  const startRow = startNode.x;
+  const startCol = startNode.y;
+  const endRow = endNode.x;
+  const endCol = endNode.y;
+
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+  const visited = Array.from({ length: numRows }, () =>
+    Array(numCols).fill(false)
+  );
+  const queue = new Queue();
+
+  const rowDirs = [-1, 0, 1, 0];
+  const colDirs = [0, 1, 0, -1];
+
+  queue.enqueue({ row: startRow, col: startCol });
+  parentMap.set(`${startRow}-${startCol}`, null);
+  visited[startRow][startCol] = true;
+
+  let reached = false;
+  while (!queue.isEmpty() && !reached) {
+    const { row, col } = queue.dequeue();
+    // console.log(`Visiting node at (${row}, ${col})`);
+
+    for (let dir = 0; dir < 4; dir++) {
+      const newRow = row + rowDirs[dir];
+      const newCol = col + colDirs[dir];
+
+      if (newRow === endRow && newCol === endCol) {
+        parentMap.set(`${endRow}-${endCol}`, { x: row, y: col });
+        let currentNode = { x: endRow, y: endCol };
+        while (true) {
+          const { x, y } = currentNode;
+          shortestPath.push({ x, y });
+          currentNode = parentMap.get(`${currentNode.x}-${currentNode.y}`);
+          if (!currentNode) break;
+        }
+        reached = true;
+        break;
+      }
+
+      if (
+        newRow >= 0 &&
+        newRow < numRows &&
+        newCol >= 0 &&
+        newCol < numCols &&
+        !matrix[newRow][newCol].isWall &&
+        !visited[newRow][newCol]
+      ) {
+        queue.enqueue({ row: newRow, col: newCol });
+        traversal.push({ x: newRow, y: newCol });
+        parentMap.set(`${newRow}-${newCol}`, { x: row, y: col });
+        visited[newRow][newCol] = true;
+      }
+    }
+  }
+
+  return { traversal, shortestPath };
+}
---
+++ algorithms/dijktras.ts
@@ -0,0 +1,90 @@
+//@ts-nocheck
+
+import { PriorityQueue } from "@/dataStructure/priorityQueue"; // Import your pq class here
+
+function createMatrix(rows, cols) {
+  return Array.from({ length: rows }, () => Array(cols).fill(100));
+}
+
+const rowDirs = [-1, 0, 1, 0];
+const colDirs = [0, 1, 0, -1];
+
+export function Dijktras(matrix, startNode, endNode) {
+  console.log("DJK...");
+  const traversal = [];
+  const shortestPath = [];
+  // Use a map to store parent nodes for each visited node
+  const parentMap = new Map();
+
+  startNode.isWall = false;
+  endNode.isWall = false;
+
+  const startRow = startNode.x;
+  const startCol = startNode.y;
+  const endRow = endNode.x;
+  const endCol = endNode.y;
+
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+  const distances = createMatrix(numRows, numCols);
+
+  distances[startRow][startCol] = 0;
+  const pq = new PriorityQueue();
+
+  parentMap.set(`${startRow}-${startCol}`, null);
+  pq.push({ node: startNode, priority: 0 });
+
+  let reached = false;
+
+  while (!pq.isEmpty() && !reached) {
+    const { node: current, priority: currentDistance } = pq.pop();
+
+    const { x, y } = current;
+
+    for (let dir = 0; dir < 4; dir++) {
+      const newRow = x + rowDirs[dir];
+      const newCol = y + colDirs[dir];
+
+      if (
+        newRow < 0 ||
+        newRow >= numRows ||
+        newCol < 0 ||
+        newCol >= numCols ||
+        matrix[newRow][newCol].isWall
+      ) {
+        continue;
+      }
+
+      if (newRow === endRow && newCol === endCol) {
+        console.log("reached");
+        parentMap.set(`${endRow}-${endCol}`, { x, y });
+        let currentNode = { x: endRow, y: endCol };
+        while (true) {
+          const { x, y } = currentNode;
+          shortestPath.push({ x, y });
+          currentNode = parentMap.get(`${currentNode.x}-${currentNode.y}`);
+          if (!currentNode) break;
+        }
+        reached = true;
+        break;
+      }
+
+      const weight = matrix[newRow][newCol].weight || 1;
+
+      const distanceThroughCurrent = currentDistance + weight;
+
+      if (distanceThroughCurrent < distances[newRow][newCol]) {
+        distances[newRow][newCol] = distanceThroughCurrent;
+        pq.push({
+          node: matrix[newRow][newCol],
+          priority: distanceThroughCurrent,
+        });
+
+        traversal.push({ x: newRow, y: newCol });
+        parentMap.set(`${newRow}-${newCol}`, { x, y });
+      }
+    }
+  }
+
+  return { traversal, shortestPath };
+}
